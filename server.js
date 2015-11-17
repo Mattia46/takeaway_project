889dd6ff49d2bb910d1ae1148a438d98ac57b447
Ezzy Elliott <ezzy.elliott@gmail.com>
2015-11-17 11:47:27 +0000

    Initial mongoose setup

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var config = require('./config/config'),
 	mongoose = require('./config/mongoose'),
 	express = require('./config/express'),
-	passport = require('./config/passport');
+	// passport = require('./config/passport');
 
 var db = mongoose(),
 	app = express(),
-	passport = passport();
+	// passport = passport();
 
 app.listen(config.port);
 
