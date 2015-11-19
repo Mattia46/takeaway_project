ed53e2e6c5d4a5d75e8c8b1c55ebb7cab47e97a0
Ezzy Elliott <ezzy.elliott@gmail.com>
2015-11-19 13:05:15 +0000

    Added user login and register, missing facebook and twitter

--- config/express.js
+++ config/express.js
@@ -2,7 +2,8 @@ var config = require('./config'),
     express = require('express'),
     passport = require('passport'),
     bodyParser = require('body-parser'),
-    flash = require('connect-flash');
+    flash = require('connect-flash'),
+    session = require('express-session');
 
 module.exports = function() {
     var app = express();
@@ -15,6 +16,11 @@ module.exports = function() {
     app.use(passport.initialize());
     app.use(passport.session());
     app.use(flash());
+    app.use(session({
+        saveUninitialized: true,
+        resave: true,
+        secret: 'OurSuperSecretCookieSecret'
+    }));
 
     app.set('views', './app/views');
     app.set('view engine', 'ejs');
