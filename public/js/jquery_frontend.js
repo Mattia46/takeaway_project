c8355665270edce171336a881436e48b49642a52
samover <samuel.overloop@gmail.com>
2015-11-19 14:36:34 +0000

    refactored

--- public/js/jquery_frontend.js
+++ public/js/jquery_frontend.js
@@ -3,41 +3,46 @@ $( document ).ready(function() {
   var menu = {};
   var order = new Order(menu);
 
-    $( "#login" ).click(function( event ) {
+  $( "#login" ).click(function( event ) {
 
-        alert( "Thanks for visiting!" );
+    alert( "Thanks for visiting!" );
 
-    });
-
-  $.getJSON("/dishes", function(res) {
-    $('#menu').append('<ul id="listmenu"></ul>');
-     res.forEach(function(dish) {
-      makeMenu(dish);
-      $('#listmenu').append("<li><button id='" +dish.name+ "'>+</button> " + dish.name +": £" + dish.price+"</li> ");
-    });
-     populateOrder();
   });
 
-  var makeMenu = function(dish) {
-    menu[dish.name]=dish.price;
-  };
+  $.getJSON("/dishes", function(data) {
+    $.each(data, function(index, dish) {
+      menu[dish.name] = dish.price;
+    });
 
-  var populateOrder = function() {
+    writeMenu(menu);
+    addOrder();            
+  });
+
+  function addOrder() {
     $("#listmenu button").click(function(event) {
-      order.add(this.id,1);
-      $('#listorder').html(function() {
-        var string = '<ul>';
-        Object.keys(order.dishes).forEach(function(dish) {
-          string += "<li>" + dish + " x" + order.dishes[dish] + "</li>";
-        });
-        string += "</ul><p>TOTAL: " + order.total().toFixed(2);
-        string += "<button>Place Order</button>";
-        return string;
-      });
+      order.add(this.id, 1);
+      writeOrder();
       event.preventDefault();
     });
-  };
+  }
 
+  function writeOrder() {
+    $('#listorder').html(function() {
+      var string = '<ul>';
+      $.each(order.dishes, function(name, qty) {
+        string += "<li>" + name + " x" + qty + "</li>";
+      });
+      string += "</ul><p>TOTAL: " + order.total().toFixed(2);
+      string += "<button>Place Order</button>";
+      return string;
+    });
+  }
 
-
+  function writeMenu(menu) {
+    $('#menu').append('<ul id="listmenu"></ul>');
+    for(var dish in menu ) {
+      $('#listmenu').append("<li><button id='" + dish + "'>+</button> " + 
+                             dish +": £" + menu[dish] + "</li> ");
+    }
+  }
 });
